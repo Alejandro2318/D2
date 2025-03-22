ff499f9a115fd9317588e833f6e76c4e26a03b15
Alejandro2318 <131904156+Alejandro2318@users.noreply.github.com>
2025-03-22 03:04:56 -0500

    Revert "Camila"

--- core/routes.php
+++ core/routes.php
@@ -4,7 +4,8 @@ function cargarControlador($controlador) {
     $nombreControlador = ucwords($controlador) . "Controller";
     $archivoControlador = "controllers/$nombreControlador.php";
     
-    if (!is_file($archivoControlador)) { // Si no existe el archivo, cargue el controlador principal
+    if(!is_file($archivoControlador)) {// Si no existe el archivo
+        // Cargue el controlador principal
         $archivoControlador = "controllers/" . CONTROLADOR_PRINCIPAL . "Controller.php";
     }
     
@@ -13,17 +14,16 @@ function cargarControlador($controlador) {
     return $control;
 }
 
-function cargarAccion($controlador, $accion, $id = null)
-{
-    if (isset($accion) && method_exists($controlador, $accion)) {
-        if ($id == null) {
+function cargarAccion($controlador, $accion, $id=null) {
+    if(isset($accion) && method_exists($controlador, $accion)) {
+        if($id == null){
             $controlador->$accion();
         } else {
             $controlador->$accion($id);
         }
+
     } else {
-        // Esto tenía un error antes. Ahora redirige a la acción principal correctamente.
-        $controlador->{ACCION_PRINCIPAL}();
+        $controlador->ACCION_PRINCIPAL;
     }
 }
 
